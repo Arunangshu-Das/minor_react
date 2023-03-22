c7505f3c3ae92bb1b90f9a7cc2d4f701b0d94415
Anik Bhattacharya <anikbhattachar27@gmail.com>
2023-03-22 18:25:39 +0530

    fourth  commit

--- src/components/navbar/Signup.jsx
+++ src/components/navbar/Signup.jsx
@@ -1,4 +1,6 @@
 import React,{useState} from 'react'
+import { Link } from 'react-router-dom';
+import "./signup.css"
 
 function Signup() {
 
@@ -6,8 +8,10 @@ function Signup() {
     firstname:"",
     lastname:"",
     email:"",
-    phoneno:"",
-    password:""
+    number:"",
+    psw:"",
+    address:"",
+    psw_repeat:""
   });
 
   const [allEntry, setAllEntry] = useState([]);
@@ -26,7 +30,7 @@ function Signup() {
     console.log(allEntry);
     setAllEntry([...allEntry, newEntry]);
     
-    setUsrSign({firstname:"",lastname:"", email:"", phoneno: "", password:""});
+    setUsrSign({firstname:"",lastname:"", email:"", number: "", psw:"",address:"",psw_repeat:"" });
   }
 
  
@@ -35,7 +39,7 @@ function Signup() {
   
   return (
     <>
-      <form action='' onSubmit={handleSubmit}>
+      {/* <form action='' onSubmit={handleSubmit}>
 
         <div>
         <label htmlFor='firstname'>First Name</label>
@@ -52,8 +56,8 @@ function Signup() {
         </div>
 
         <div>
-        <label htmlFor='phoneno'>Phone</label>
-        <input type="text" name="phoneno" id="phoneno" value={userSign.phoneno} onChange={handleInput}/>
+        <label htmlFor='number'>Phone</label>
+        <input type="text" name="number" id="number" value={userSign.number} onChange={handleInput}/>
         </div>
 
         <div>
@@ -62,9 +66,45 @@ function Signup() {
         </div>
 
         <button type='submit'>Sign Up</button>
-      </form>
+      </form> */}
+
+<form action=" " onSubmit={handleSubmit}>
+		<h1>Sign Up</h1>
+		<p>Please fill in this form to create an account.</p>
+		<hr/>
+		
+		<label for="name"><b>First Name</b></label>
+		<input type="text" placeholder="Enter Your Name" name="firstname" value={userSign.firstname} onChange={handleInput} required/><br/>
+
+        <label for="name"><b>Last Name</b></label>
+		<input type="text" placeholder="Enter Your Name" name="lastname" value={userSign.lastname} onChange={handleInput} required/><br/>
+
+        <label for="name"><b>E-Mail</b></label>
+		<input type="email" placeholder="Enter Your Email Id" name="email" value={userSign.email} onChange={handleInput} required/><br/>
+		
+		<label for="number"><b>Contact No.</b></label>
+		<input type="number" placeholder="Enter Your Contact No." name="number" value={userSign.number} onChange={handleInput} required/><br/>
+		
+		<label for="email"><b>Address</b></label> 
+		<input type="text" placeholder="Enter Your Full Address" name="address" value={userSign.address} onChange={handleInput} required/><br/>
+
+		<label for="pwd"><b>Password</b></label>
+		<input type="password" placeholder="Enter Your Password" name="psw" value={userSign.psw} onChange={handleInput} required/><br/>
+
+		<label for="psw-repeat"><b>Confirm Password</b></label>
+		<input type="password" placeholder="Repeat Your Password" name="psw_repeat" value={userSign.psw_repeat} onChange={handleInput} required/><br/>
+		<hr/>
+		<p>By creating an account you agree to our <Link to="#">Terms & Privacy</Link>.</p>
+
+		<button type="submit" class="btn">Register</button>
+		
+  
+		
+		<p>Already have an account? <Link to="/login">Sign in</Link></p>
+		
+	</form>
       
-       </>
+  </>
   )
 }
 
